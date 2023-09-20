b43c54d87978a1ec45bf7fa30bab9919616fc903
RafaGomesReis <134978695+RafaGomesReis@users.noreply.github.com>
2023-09-20 20:39:10 -0300

    subindo_aulas_anteriores

---
+++ javascript-testes/12-9-2023/script.js
@@ -0,0 +1,10 @@
+// let x = 0
+
+// x = x +1
+
+// console.log(x)
+let nome= 'Rafael'
+let sobrenome='Reis'
+let nomecompleto = `Rafael Gomes`
+
+console.log(nomecompleto)
---
+++ javascript-testes/assets/js/script.js
@@ -0,0 +1,9 @@
+let nomecarro = "ferrari"
+console.log(nomecarro)
+
+let preçodobolo= "19,99"
+console.log(preçodobolo)
+
+let cidade = "São PAulo"
+console.log(cidade)
+

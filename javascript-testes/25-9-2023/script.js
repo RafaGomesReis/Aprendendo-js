9dca25eb84980bccc55547ba2eea27600562df88
Rafael <raf.gomes.reis@gmail.com>
2023-09-26 09:32:33 -0300

    subindo_dia_25-9-2023

---
+++ javascript-testes/25-9-2023/script.js
@@ -0,0 +1,80 @@
+
+
+
+// // // function somar(x, y) {
+// // //     return x + y;
+// // // }
+// // const somar = (x, y) =>{
+// //     return x + y;
+// // }
+// console.log(somar(10, 11));
+
+
+// function add(){
+
+//     count++;
+// }
+// let count = 0;
+
+// add();
+
+// add();
+
+// console.log(count);
+
+
+// console.log(circle(2));
+
+// function addCircles(a, b){
+//     // function circle(x){
+//     //     return x * x;
+//     // }
+//     const circle = (x) => x * x;
+
+//     cA = circle(a);
+//     cB = circle(b);
+//     return cA + cB;
+// }
+// console.log(addCircles(2, 3));
+
+
+// let colors = ['red', 'green', 'blue'];
+
+// console.log(colors[0]);
+
+// let list = ['sabugo', 22, 'cirilo'];
+
+// let lista = ['cerejo', 'tangerino', list ];
+
+// console.log(lista[2][0]);
+
+
+// let ingredientes =[
+//     'farinha', 'água','ovo','sal'
+// ];
+
+// ingredientes.push = 'cebola';
+
+// console.log(ingredientes);
+
+// console.log(`Total :${ ingredientes.length}`);
+
+let carros = ['BMW', 'Ferrari', 'Mercedes']
+    x =[1]
+    console.log('1.' + carros[x])
+    console.log(x[1])
+
+
+    carros[1] = "Audi"
+    console.log(carros)
+    console.log('2. Lista com Audi: ' + carros);
+
+
+    carros.push('Volvo');
+    console.log(carros)
+    console.log('lista com Volvo: ' + carros)
+
+    
+    console.log(`4 items no Array : ${carros.length}`);
+
+
